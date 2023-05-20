d8e7f6162cee3ed1490a5a70602714c32c5f248b
Mouath104 <89397795+Mouath104@users.noreply.github.com>
2023-05-19 23:39:05 -0800

    Create TODO-APP.mjs

---
+++ TODO-APP.mjs
@@ -0,0 +1,5 @@
+let q=-1
+while(q!=="q"){
+    console.log("***************************\nWelcome to JS TODO-APP\n***************************\nSelect an action:\n1) Add a new task\n2) List all tasks\n3) List completed tasks\n4) Mark the task as done\n5) Delete a task\n6) Sort tasks by the due date\n7) Sort tasks by priority\n8) Clear all tasks\n9) Quit***************************\nWhat's your choice?\n")
+    q = prompt("Enter Your Choice: ");
+}
